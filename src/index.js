50cfbbf1d728dcbe25e3fbdde058f8a0363ede4d
nchernyshov <xpom55@mail.ru>
2021-05-12 04:25:37 +0300

    feat: react-refresh-webpack does not work correctly

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// issue: https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/main/docs/TROUBLESHOOTING.md
+
+window.$RefreshReg$ = () => {};
+window.$RefreshSig$ = () => (type) => type;
